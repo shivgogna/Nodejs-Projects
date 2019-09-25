d861e36f474073ec5ee1d48b01743307888264c0
Shiv Gogna <sgogna0912@gmail.com>
2019-09-25 15:11:46 -0500

    Add user input from command line

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,53 @@
-const validator = require('validator');
-const notes = require('./notes.js');
+const chalk = require("chalk");
+const yargs = require("yargs");
 
-const msg = notes()
-console.log(msg)
+const notes = require("./notes.js");
 
-console.log(validator.isEmail('shiv@example.com'))
+//Customize yargs verison
+yargs.version("1.1.0");
+
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+        describe: 'Note title',
+        demandOption: true,
+        type: 'string'
+    },
+    body: {
+        describe: "Note Body",
+        demandOption: true,
+        type: 'string'
+    }
+},
+  handler: function(argv) {
+    console.log(`Title: ${argv.title} \nBody: ${argv.body}`);
+  }
+});
+
+yargs.command({
+    command: "remove",
+    describe: "Remove a new note",
+    handler: function(argv) {
+        console.log("Removing note", argv);
+    }
+});
+
+yargs.command({
+    command: "read",
+    describe: "Read a note",
+    handler: function() {
+        console.log("Reading note")
+    }
+});
+
+yargs.command({
+    command: "list",
+    describe: "List all notes",
+    handler: function() {
+        console.log("Listing all notes")
+    }
+});
+
+yargs.parse();
