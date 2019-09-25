4f267fd0a05176c08fc8bb4148a1c291f9bf4b11
Shiv Gogna <sgogna0912@gmail.com>
2019-09-25 15:27:50 -0500

    Play with JSON data to stringify and parse

---
+++ playgrounds/1-json.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday'
+// }
+
+// const bookJSON = JSON.stringify(book);
+// console.log(bookJSON)
+
+// const parsedData = JSON.parse(bookJSON)
+// console.log(parsedData.author)
+
+// fs.writeFileSync('1-json.json', bookJSON)
+
+
+// const dataBuffer = fs.readFileSync('1-json.json')
+// const dataJSON = dataBuffer.toString()
+// const data = JSON.parse(dataJSON)
+// console.log(data.title)
+
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJSON = dataBuffer.toString()
+const user = JSON.parse(dataJSON)
+
+user.name = 'Shivinder'
+user.age = 30
+
+const userJSON = JSON.stringify(user)
+fs.writeFileSync('1-json.json', userJSON)
