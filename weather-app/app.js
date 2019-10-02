6cd88f63fc9eddcaf1b3374d2ae06ca890872f46
Shiv Gogna <sgogna0912@gmail.com>
2019-10-02 12:21:51 -0500

    explore data using json and added examples

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,11 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast//37.8267,-122.4233'
+const url = `https://api.darksky.net/forecast//37.8267,-122.4233?units=us`
 
-request({ url: url }, (error, response) => {
-    console.log(response)
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ 
+    url: url, 
+    json: true 
+}, (error, response) => {
+    console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.`)
 })
+
