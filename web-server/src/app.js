d5d22e066ba26171676655dfae769d2479e750f6
Shiv Gogna <sgogna0912@gmail.com>
2019-10-03 11:46:20 -0500

    created hbs files to replace static files

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,9 +3,38 @@ const express = require('express')
 
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+//DEFINE PATHS FOR EXPRESS CONFIG
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+//SETUP HANDLEBARS ENGINE AND VIEWS LOCATION
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+//SETUP STATIC DIRECTORY TO SERVE
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Shiv Gogna'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: "About Me",
+        name: 'Darth Vader',
+        img: './img/DV'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('./help', {
+        title: 'Help'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
