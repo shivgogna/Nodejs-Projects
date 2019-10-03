4229de69ff214a00b5fbd9d212f9b95046e4c418
Shiv Gogna <sgogna0912@gmail.com>
2019-10-03 15:27:48 -0500

    query string

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -41,10 +41,28 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+    if(!req.query.address) {
+        return res.send({
+            error: 'You must provide a valid address!'
+        })
+    }
     res.send({
-        title: 'Weather',
+        forecast: 'It is bright and sunny',
         location: 'Corona',
-        forecast: 'It is bright and sunny'
+        address: req.query.address
+    })
+})
+
+app.get('/products', (req, res) => {
+    if(!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: []
     })
 })
 
