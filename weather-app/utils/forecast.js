90ef985e9deb922775df95261bf2a7f09598f69a
Shiv Gogna <sgogna0912@gmail.com>
2019-10-03 09:00:29 -0500

    destructed objects and added es6 syntax

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,16 +1,17 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = `https://api.darksky.net/forecast//${latitude},${longitude}?units=us`;
+    //ADD URL WITH KEY
+    const url = ``;
 
     
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback("Unable to conenct to weather service!", undefined);
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback("Unable to conenct to weather service!", undefined);
         } else {
-            callback( undefined, `${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.`
+            callback( undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`
         );
         }
     });
