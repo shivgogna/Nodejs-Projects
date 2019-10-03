17267266b3939a96a37bfe06d419424ff3eb75f1
Shiv Gogna <sgogna0912@gmail.com>
2019-10-03 11:19:43 -0500

    added html and js

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,17 @@
+const path = require('path')
 const express = require('express')
 
+
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.get('', (req, res) => {
-    res.send('Hello express')
-})
-
-app.get('/help', (req, res) => {
-    res.send('Help page')
-})
-
-app.get('/about', (req, res) => {
-    res.send('About Me')
-})
+app.use(express.static(publicDirectoryPath))
 
 app.get('/weather', (req, res) => {
-    res.send('Weather')
+    res.send({
+        location: 'Corona',
+        forecast: 'It is bright and sunny'
+    })
 })
 
 app.listen(3000, () => {
