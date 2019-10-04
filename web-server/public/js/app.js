633e98a254307a3073c743aa8150e7a5253db3d3
Shiv Gogna <sgogna0912@gmail.com>
2019-10-04 16:14:04 -0500

    added search form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,22 @@
 console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    
+    
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if(data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
+    })
+})
