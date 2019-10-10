0010a0f6b88a8e6a5b2131ba1877aaf3362c4872
Shiv Gogna <sgogna0912@gmail.com>
2019-10-10 16:13:17 -0500

    task route

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -14,6 +15,16 @@ app.post('/users', (req, res) => {
         res.send(user)
     }).catch((e) => {
         res.status(400).send(e)
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+
+    task.save().then(() => {
+        res.send(task)
+    }).catch((e) => {
+        res.status(400).semd(e)
     })
 })
 
